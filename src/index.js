2fb529867e8fff581673a37b5253e13ced7b8eb8
GokilaSundar <gokithilaga267@gmail.com>
2025-02-07 23:14:08 +0530

    Create express app and setup middlewares

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+import cookieParser from "cookie-parser";
+import express from "express";
 import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.json()); // Parse body from JSON payload
+app.use(cookieParser()); // Parse cookies as object from header
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/auth-sample?directConnection=true")
   .then(() => {
     console.log("Database connected!");
+
+    app.listen(5000, () => {
+      console.log("Server started at 5000!");
+    });
   })
   .catch((error) => {
     console.error("Failed to connect to databse", error);
